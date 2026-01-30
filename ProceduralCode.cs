815d1c4e1d10843f9199c775f7362e9b743baa4d
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-30 12:15:29 -0600

    Update ProceduralCode.cs

--- ProceduralCode.cs
+++ ProceduralCode.cs
@@ -6,16 +6,18 @@ public class ProceduralCode
     private int _length;
     private int _height;
     
-    public class Rectangle (int length, int height) 
+    public class Rectangle(int length, int height) 
     {
             _length = length;
             _height = height;
     }
 
-    public int CalculateArea() {
-            return _length * _height;
-        }
+    public int CalculateArea() 
+    {
+        return _length * _height;
+    }
 }
+
 
 
 
